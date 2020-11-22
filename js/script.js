637c337dd297a911a8e4ad3405a5cc3b6dec845d
Andrew Sipes <andrew.n.sipes@gmail.com>
2020-11-21 19:19:33 -0500

    Added better defaults.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-document.querySelector("#name").innerText = urlParams.get("name");
-document.querySelector("#code").innerText = urlParams.get("code");
+document.querySelector("#name").innerText = urlParams.get("name") || "________";
+document.querySelector("#code").innerText =
+  urlParams.get("code") || "_____________________";
